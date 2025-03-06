1b516fb9e4501e47769cad8cd08fdb86b4cce254
Alfiqo Jundullah <alfiqo.jundullah@paper.id>
2025-03-07 04:57:55 +0700

    add validation only 1 clockin per day

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -3,6 +3,7 @@ class SleepRecord < ApplicationRecord
 
   before_update :calculate_duration
   validate :woke_at_not_changed, on: :update
+  validate :one_slept_at_per_day, on: :create
 
   private
 
@@ -15,6 +16,12 @@ class SleepRecord < ApplicationRecord
   def woke_at_not_changed
     if woke_at_changed? && woke_at_was.present?
       errors.add(:woke_at, "can only be set once")
+    end
+  end
+
+  def one_slept_at_per_day
+    if user.sleep_records.where("DATE(slept_at) = ?", slept_at.to_date).exists?
+      errors.add(:slept_at, "can only have one record per day")
     end
   end
 end
