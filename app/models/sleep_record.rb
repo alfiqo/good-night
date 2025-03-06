23c106521b47b8a309bb9a2508ec674f29b0ced0
Alfiqo Jundullah <alfiqo.jundullah@paper.id>
2025-03-07 04:54:31 +0700

    add validation woke_at is not changed

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -2,6 +2,7 @@ class SleepRecord < ApplicationRecord
   belongs_to :user
 
   before_update :calculate_duration
+  validate :woke_at_not_changed, on: :update
 
   private
 
@@ -9,5 +10,11 @@ class SleepRecord < ApplicationRecord
     return if slept_at.blank? || woke_at.blank?
 
     self.duration_minutes = ((woke_at - slept_at) / 60).to_i
+  end
+
+  def woke_at_not_changed
+    if woke_at_changed? && woke_at_was.present?
+      errors.add(:woke_at, "can only be set once")
+    end
   end
 end
