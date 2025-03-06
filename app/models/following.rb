fe7deae97f5d846150842de3b90436bab90fff3a
Alfiqo Jundullah <alfiqo.jundullah@paper.id>
2025-03-07 04:50:26 +0700

    add validation only follow once and cannot follow self user

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,4 +1,13 @@
 class Following < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+
+  validate :cannot_follow_self
+  validates :follower_id, uniqueness: { scope: :followed_id, message: "can only follow a user once" }
+
+  private
+
+  def cannot_follow_self
+    errors.add(:follower_id, "can't follow yourself") if follower_id == followed_id
+  end
 end
--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -2,12 +2,12 @@ class SleepRecord < ApplicationRecord
   belongs_to :user
 
   before_update :calculate_duration
-  
+
   private
 
   def calculate_duration
-    return if slept_at.blank? || woke_at.blank? # Ensure both timestamps are present
+    return if slept_at.blank? || woke_at.blank?
 
-    self.duration_minutes = ((woke_at - slept_at) / 60).to_i # Convert seconds to minutes
+    self.duration_minutes = ((woke_at - slept_at) / 60).to_i
   end
 end
